516c0d77c2356adeeac7dc9bafa90a9b620b60d4
Heger Arias Santos <hegel.0218@gmail.com>
2023-05-26 23:35:26 -0400

    structure finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,68 @@
+"use client";
+import { Example } from "@/components";
+import { decrement, increment, reset } from "@/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useGetUsersQuery } from "@/redux/services/userApi";
 import Link from "next/link";
-
 export default function Home() {
+  const urlTemplates =
+    "https://tailwindtemplates.co/templates/tags/startup?type=free";
+
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
+  const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home Page
-      <Link href={"/home"}>Go to page</Link>
-      <a href="https://tailwindtemplates.co/templates/tags/startup?type=free">
-        Templates
-      </a>
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+
+      {error ? (
+        <p>Oh no, there was an error</p>
+      ) : isLoading || isFetching ? (
+        <p>Loading...</p>
+      ) : data ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gap: 20,
+          }}
+        >
+          {data.map((user) => (
+            <div
+              key={user.id}
+              style={{ border: "1px solid #ccc", textAlign: "center" }}
+            >
+              <img
+                src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                alt={user.name}
+                width={180}
+                height={180}
+              />
+              <h3>{user.name}</h3>
+            </div>
+          ))}
+        </div>
+      ) : null}
+
+      <Example />
+
+      <div className="flex justify-center gap-4 items-center mt-8">
+        <a href={urlTemplates}>Look templates</a>
+
+        <Link href="/home">Go to home (multiPage)</Link>
+      </div>
     </main>
   );
 }
